b76689be01e7a7082e71d3eaab5728e641e211be
zuoqiang <756465185@qq.com>
2019-11-30 23:50:00 +0800

    我的第一个基础网关，转发任意路径到 百度

---
+++ src/main/java/com/zuoqiang/mygateway/MyGatewayApplication.java
@@ -0,0 +1,18 @@
+package com.zuoqiang.mygateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 自定义网关启动类
+ *
+ * @author zuoqiang
+ */
+@SpringBootApplication
+public class MyGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zuoqiang/mygateway/MyGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zuoqiang.mygateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
